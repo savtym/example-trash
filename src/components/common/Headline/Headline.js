6a7eb8e87a25df891a2bdf97ec5809e39ba19f60
dev6 <root@dev6>
2019-02-22 16:51:40 +0200

    new branch

--- src/components/common/Headline/Headline.js
+++ src/components/common/Headline/Headline.js
@@ -4,6 +4,7 @@ import Styles from './Headline.module.scss';
 function Headline(props) {
     const { children } = props;
 
+
     return (
         <h1 className={Styles.headline}> {children} </h1>
     )
