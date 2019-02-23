ce15d3b101c883fb71b23626a12e27101964bf41
Oleksii <mysterious240@gmail.com>
2019-02-23 19:54:05 +0200

    fff

--- src/components/core/Main/Main.js
+++ src/components/core/Main/Main.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Styles from './Main.module.scss';
 
 import {
-	Button,
-	TextField,
-	TypesButton,
-	Select,
-	Headline,
+    Button,
+    TextField,
+    TypesButton,
+    Select,
+    Headline,
 } from '../../common';
 
 import { options as selectOptions } from './data';
@@ -14,35 +14,35 @@ import { options as selectOptions } from './data';
 
 class Main extends Component {
 
-	state = {
-		selected: '2',
-	};
+    state = {
+        selected: '2',
+    };
 
-	onChangeSelectedName = ({ target }) => {
-		// target is html tag <select />
-		// target.value - selected option
-		// setState: https://reactjs.org/docs/react-component.html#setstate
-		this.setState({
-			selected: target.value,
-		});
-	};
+    onChangeSelectedName = ({ target }) => {
+        // target is html tag <select />
+        // target.value - selected option
+        // setState: https://reactjs.org/docs/react-component.html#setstate
+        this.setState({
+            selected: target.value,
+        });
+    };
 
-	render() {
-		const { selected } = this.state;
+    render() {
+        const { selected } = this.state;
 
-		const user = selectOptions.find((option) => option.value === selected);
+        const user = selectOptions.find((option) => option.value === selected);
 
-		return (
-			<div className={Styles.wrapper}>
-				<span>Hello World!</span>
+        return (
+            <div className={Styles.wrapper}>
+                <span>Hello World!</span>
 
-				<Button type={TypesButton.error}>
-					<TextField/>
-				</Button>
+                <Button type={TypesButton.error}>
+                    <TextField/>
+                </Button>
 
-				<Button type={TypesButton.success}>
-					<span className={Styles.error}>Error</span>
-				</Button>
+                <Button type={TypesButton.success}>
+                    <span className={Styles.error}>Error</span>
+                </Button>
 
                 <Headline>
                     <span>
@@ -50,19 +50,18 @@ class Main extends Component {
                     </span>
                 </Headline>
 
-				<TextField/>
+                <TextField/>
 
-				<Select
-					onChange={this.onChangeSelectedName}
-					options={selectOptions}
-					defaultValue={selected}
-				/>
+                <Select
+                    onChange={this.onChangeSelectedName}
+                    options={selectOptions}
+                    defaultValue={selected}
+                />
 
-				<h2 className={Styles.title}>Hi, {user.label}!</h2>
-
-			</div>
-		);
-	}
+                <h2 className={Styles.title}>Hi, {user.label}!</h2>
+            </div>
+        );
+    }
 }
 
 
