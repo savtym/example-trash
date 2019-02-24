19c32ba00ba93052c2d8e6cc7235673c0d81fc70
Oleksii <mysterious240@gmail.com>
2019-02-24 11:54:38 +0200

    dff

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,4 @@
 export { default as Button, TypesButton } from './Button';
 export { default as TextField } from './TextField';
-// export { default as Select } from './Select';
+export { default as Select } from './Select';
 export { default as Headline } from './Headline';
